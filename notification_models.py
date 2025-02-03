ae27980d69dbf406a4cf11146190eee774237f20
Alfred <andychen208@gmail.com>
2025-02-03 21:14:05 +0800

    refactor: 重構observer model

--- notification_models.py
+++ notification_models.py
@@ -1,4 +1,6 @@
 import logging
+from abc import ABC, abstractmethod
+
 from line_notification import LineNotification
 from utils import trans_temp_k2c
 
@@ -10,7 +12,7 @@ def get_notification_model(model_type):
     else:
         return GeneralModelNotification
     
-def commom_message_formatter(msg):
+def common_message_formatter(msg):
     return {
         'temperature': trans_temp_k2c(
             msg.get('temperature', {}).get('temp', 'Null')),
@@ -18,40 +20,43 @@ def commom_message_formatter(msg):
         'status': msg.get('status', 'Null'),
     }
 
-
-class GeneralModelNotification(LineNotification):
+class NotificationBase(ABC, LineNotification):
+    """通知基類（抽象類別），提供共用的初始化和 notify 方法"""
+    
     def __init__(self, channel_token, user_data):
         super().__init__(channel_token)
         self.user_data = user_data
-        
+
+    @abstractmethod
     def _complete_message(self, msg):
-        return str({
-            'user': self.user_data.get('user', 'Null'),
-            **commom_message_formatter(msg)
-        })
+        """強制子類別實作"""
+        pass
 
     def notify(self, msg):
-        logger.info('user: {} GeneralModel notify message: {}'.format(
-            self.user_data.get('user', 'NAME_IS_NEEDED'), self._complete_message(msg)))
-        return super().notify(self.user_data['user_id'],
-            self._complete_message(msg))
+        """發送通知"""
+        complete_message = self._complete_message(msg)
+        logger.info('user: {} notify message: {}'.format(
+            self.user_data.get('user', 'NAME_IS_NEEDED'), complete_message))
+        return super().notify(self.user_data['user_id'], complete_message)
 
 
-class PremiumModelNotification(LineNotification):
-    def __init__(self, channel_token, user_data):
-        super().__init__(channel_token)
-        self.user_data = user_data
-        
+class GeneralModelNotification(NotificationBase):
+    """一般會員通知"""
+    
     def _complete_message(self, msg):
         return str({
             'user': self.user_data.get('user', 'Null'),
-            **commom_message_formatter(msg),
+            **common_message_formatter(msg)
+        })
+
+
+class PremiumModelNotification(NotificationBase):
+    """高級會員通知"""
+    
+    def _complete_message(self, msg):
+        return str({
+            'user': self.user_data.get('user', 'Null'),
+            **common_message_formatter(msg),
             'detailed_status': msg.get('detailed_status', 'Null'),
             'wind speed': msg.get('wind', {}).get('speed', 'Null')
         })
-
-    def notify(self, msg):
-        logger.info('user: {} GeneralModel notify message: {}'.format(
-            self.user_data.get('user', 'NAME_IS_NEEDED'), self._complete_message(msg)))
-        return super().notify(self.user_data['user_id'],
-            self._complete_message(msg))
